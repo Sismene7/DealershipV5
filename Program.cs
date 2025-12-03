95137d5b27bbe839c08df3ea2fc25e1862d65ee7
Renan Ribeiro <renan.beirohotmail.com@gmail.com>
2025-12-03 17:20:05 -0300

    Alteração interface

--- Program.cs
+++ Program.cs
@@ -1,44 +1,41 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
+<<<<<<< HEAD
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Dealership MIDIAS 🚗🚗🚗🚗🚗🚗🚗",
+        Version = "♾️"
+    });
+});
+
+=======
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+>>>>>>> 279e1f6b4f994817ff7e64672f5664fb5791418a
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+<<<<<<< HEAD
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.DocumentTitle = "Minha API - Documentação";
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+    });
+=======
     app.UseSwagger();
     app.UseSwaggerUI();
     app.MapOpenApi();
+>>>>>>> 279e1f6b4f994817ff7e64672f5664fb5791418a
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
